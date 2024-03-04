e9bb767c3abcc73c14f30c4694a6a0c66928e774
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-03-03 21:16:36 -0300

    composer and monolog

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
 
-phpinfo();
+require('../vendor/autoload.php');
+
+use App\Logger;
+use App\Shit;
+
+
+$shit = new Shit();
+
+echo $shit->message();
+
+(new Logger())->write('errouu');
---
+++ src/Shit.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class Shit
+{
+    public function message(): string
+    {
+        return 'this class is a shit';
+    }
+}
